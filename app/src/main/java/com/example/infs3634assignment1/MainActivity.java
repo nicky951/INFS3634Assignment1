a564ad3c375dc5ae3a1cdce70d7c484a73fb3a9b
Nicholas Liang <z5215234@student.unsw.edu.au>
2019-10-03 16:38:15 +1000

    Updated logic without terrible formulas

--- app/src/main/java/com/example/infs3634assignment1/MainActivity.java
+++ app/src/main/java/com/example/infs3634assignment1/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -18,43 +21,26 @@ public class MainActivity extends AppCompatActivity {
     private TextView sydneyTime;
     TimeZone sydney = TimeZone.getTimeZone("Australia/Sydney");
     Calendar s = Calendar.getInstance(sydney);
-    int hourOfDaysSyd = s.get(Calendar.HOUR_OF_DAY);
-    String sydneyTimeString = hourOfDaysSyd + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
 
     //Japan Time
     private TextView tokyoTime;
     TimeZone tokyo = TimeZone.getTimeZone("Asia/Tokyo");
     Calendar t = Calendar.getInstance(tokyo);
-    int hourOfDaysTok = t.get(Calendar.HOUR_OF_DAY);
-    String tokyoTimeString = hourOfDaysTok + ":" + String.format(Locale.ENGLISH, "%02d", t.get(Calendar.MINUTE));
 
     //Auckland Time
     private TextView aucklandTime;
     TimeZone auckland = TimeZone.getTimeZone("Pacific/Auckland");
     Calendar a = Calendar.getInstance(auckland);
-    int hourOfDaysAuk = a.get(Calendar.HOUR_OF_DAY);
-    String aucklandTimeString = hourOfDaysAuk + ":" + String.format(Locale.ENGLISH, "%02d", a.get(Calendar.MINUTE));
 
     //Dubai Time
     private TextView dubaiTime;
     TimeZone dubai = TimeZone.getTimeZone("Asia/Dubai");
     Calendar d = Calendar.getInstance(dubai);
-    int hourOfDaysDub = d.get(Calendar.HOUR_OF_DAY);
-    String dubaiTimeString = hourOfDaysDub + ":" + String.format(Locale.ENGLISH, "%02d", d.get(Calendar.MINUTE));
 
     //New York Time
     private TextView newyorkTime;
     TimeZone newyork = TimeZone.getTimeZone("America/New_York");
     Calendar n = Calendar.getInstance(newyork);
-    int hourOfDaysNew = n.get(Calendar.HOUR_OF_DAY);
-    String newyorkTimeString = hourOfDaysNew + ":" + String.format(Locale.ENGLISH, "%02d", n.get(Calendar.MINUTE));
-
-    //Indicate whether AM or PM after change
-    boolean amS;
-    boolean amT;
-    boolean amA;
-    boolean amD;
-    boolean amN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,9 +57,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 if (changeTime.isChecked()) {
-                    twelveHour();
-                } else if (!changeTime.isChecked()) {
                     twentyFourHour();
+                } else if (!changeTime.isChecked()) {
+                    twelveHour();
                 }
             }
         });
@@ -81,166 +67,108 @@ public class MainActivity extends AppCompatActivity {
 
     //24 Hour Display Logic
     public void twentyFourHour() {
-        if (!amS) {
-            hourOfDaysSyd = hourOfDaysSyd + 12;
-            String sydneyTimeString = hourOfDaysSyd + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            sydneyTime.setText(sydneyTimeString);
-        } else {
-            String sydneyTimeString = hourOfDaysSyd + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            sydneyTime.setText(sydneyTimeString);
-        }
 
-        if (!amT) {
-            hourOfDaysTok = hourOfDaysTok + 12;
-            String tokyoTimeString = hourOfDaysTok + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            tokyoTime.setText(tokyoTimeString);
-        } else {
-            String tokyoTimeString = hourOfDaysTok + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            tokyoTime.setText(tokyoTimeString);
-        }
+        DateFormat twelveHour = new SimpleDateFormat("HH:mm", Locale.ENGLISH);
+        twelveHour.setTimeZone(sydney);
 
-        if (!amA) {
-            hourOfDaysAuk = hourOfDaysAuk + 12;
-            String aucklandTimeString = hourOfDaysAuk + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            aucklandTime.setText(aucklandTimeString);
-        } else {
-            String aucklandTimeString = hourOfDaysAuk + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            aucklandTime.setText(aucklandTimeString);
-        }
+        String timeS = twelveHour.format(s.getTime());
+        sydneyTime = findViewById(R.id.sydneytime);
+        sydneyTime.setText(timeS);
 
-        if (!amD) {
-            hourOfDaysDub = hourOfDaysDub + 12;
-            String dubaiTimeString = hourOfDaysDub + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            dubaiTime.setText(dubaiTimeString);
-        } else {
-            String dubaiTimeString = hourOfDaysDub + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            dubaiTime.setText(dubaiTimeString);
-        }
+        twelveHour.setTimeZone(tokyo);
 
-        if (!amN) {
-            hourOfDaysNew = hourOfDaysNew + 12;
-            String newyorkTimeString = hourOfDaysNew + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            newyorkTime.setText(newyorkTimeString);
-        } else {
-            String newyorkTimeString = hourOfDaysNew + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE));
-            newyorkTime.setText(newyorkTimeString);
-        }
+        String timeT = twelveHour.format(t.getTime());
+        tokyoTime = findViewById(R.id.tokyotime);
+        tokyoTime.setText(timeT);
+
+        twelveHour.setTimeZone(auckland);
+
+        String timeA = twelveHour.format(a.getTime());
+        aucklandTime = findViewById(R.id.aucklandtime);
+        aucklandTime.setText(timeA);
+
+        twelveHour.setTimeZone(dubai);
+
+        String timeD = twelveHour.format(d.getTime());
+        dubaiTime = findViewById(R.id.dubaitime);
+        dubaiTime.setText(timeD);
+
+        twelveHour.setTimeZone(newyork);
+
+        String timeN = twelveHour.format(n.getTime());
+        newyorkTime = findViewById(R.id.newyorktime);
+        newyorkTime.setText(timeN);
+
     }
 
     //Display 12 Hour time from 24 Hour logic
     public void twelveHour() {
-        if (hourOfDaysSyd > 12) {
-            amS = false;
-            hourOfDaysSyd = hourOfDaysSyd - 12;
-            String sydneyTimeString = hourOfDaysSyd + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            sydneyTime.setText(sydneyTimeString);
-        } else if(hourOfDaysSyd == 0) {
-            amS = true;
-            String sydneyTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            sydneyTime.setText(sydneyTimeString);
-        } else if(hourOfDaysSyd == 12) {
-            amS = true;
-            String sydneyTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            sydneyTime.setText(sydneyTimeString);
-        } else {
-            amS = true;
-            String sydneyTimeString = hourOfDaysSyd + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            sydneyTime.setText(sydneyTimeString);
-        }
 
-        if (hourOfDaysTok > 12) {
-            amT = false;
-            hourOfDaysTok = hourOfDaysTok - 12;
-            String tokyoTimeString = hourOfDaysTok + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            tokyoTime.setText(tokyoTimeString);
-        } else if(hourOfDaysTok == 0) {
-            amT = true;
-            String tokyoTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            tokyoTime.setText(tokyoTimeString);
-        }  else if(hourOfDaysTok == 12) {
-            amT = true;
-            String tokyoTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            tokyoTime.setText(tokyoTimeString);
-        } else {
-            amT = true;
-            String tokyoTimeString = hourOfDaysTok + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            tokyoTime.setText(tokyoTimeString);
-        }
+        DateFormat twelveHour = new SimpleDateFormat("hh:mm aa", Locale.ENGLISH);
+        twelveHour.setTimeZone(sydney);
 
-        if (hourOfDaysAuk > 12) {
-            amA = false;
-            hourOfDaysAuk = hourOfDaysAuk - 12;
-            String aucklandTimeString = hourOfDaysAuk + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            aucklandTime.setText(aucklandTimeString);
-        } else if(hourOfDaysAuk == 0) {
-            amA = true;
-            String aucklandTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            aucklandTime.setText(aucklandTimeString);
-        }  else if(hourOfDaysAuk == 12) {
-            amA = true;
-            String aucklandTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            aucklandTime.setText(aucklandTimeString);
-        } else {
-            amA = true;
-            String aucklandTimeString = hourOfDaysAuk + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            aucklandTime.setText(aucklandTimeString);
-        }
+        String timeS = twelveHour.format(s.getTime());
+        sydneyTime = findViewById(R.id.sydneytime);
+        sydneyTime.setText(timeS);
 
-        if (hourOfDaysDub > 12) {
-            amD = false;
-            hourOfDaysDub = hourOfDaysDub - 12;
-            String dubaiTimeString = hourOfDaysDub + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            dubaiTime.setText(dubaiTimeString);
-        } else if(hourOfDaysDub == 0) {
-            amD = true;
-            String dubaiTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            dubaiTime.setText(dubaiTimeString);
-        }  else if(hourOfDaysDub == 12) {
-            amD = true;
-            String dubTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            dubaiTime.setText(dubTimeString);
-        } else {
-            amD = true;
-            String dubaiTimeString = hourOfDaysDub + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            dubaiTime.setText(dubaiTimeString);
-        }
+        twelveHour.setTimeZone(tokyo);
 
-        if (hourOfDaysNew > 12) {
-            amN = false;
-            hourOfDaysNew = hourOfDaysNew - 12;
-            String newyorkTimeString = hourOfDaysNew + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            newyorkTime.setText(newyorkTimeString);
-        } else if(hourOfDaysNew == 0) {
-            amN = true;
-            String newyorkTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            newyorkTime.setText(newyorkTimeString);
-        }  else if(hourOfDaysNew == 12) {
-            amN = true;
-            String newyorkTimeString =  "12:" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " pm";
-            newyorkTime.setText(newyorkTimeString);
-        } else {
-            amN = true;
-            String newyorkTimeString = hourOfDaysNew + ":" + String.format(Locale.ENGLISH, "%02d", s.get(Calendar.MINUTE)) + " am";
-            newyorkTime.setText(newyorkTimeString);
-        }
+        String timeT = twelveHour.format(t.getTime());
+        tokyoTime = findViewById(R.id.tokyotime);
+        tokyoTime.setText(timeT);
+
+        twelveHour.setTimeZone(auckland);
+
+        String timeA = twelveHour.format(a.getTime());
+        aucklandTime = findViewById(R.id.aucklandtime);
+        aucklandTime.setText(timeA);
+
+        twelveHour.setTimeZone(dubai);
+
+        String timeD = twelveHour.format(d.getTime());
+        dubaiTime = findViewById(R.id.dubaitime);
+        dubaiTime.setText(timeD);
+
+        twelveHour.setTimeZone(newyork);
+
+        String timeN = twelveHour.format(n.getTime());
+        newyorkTime = findViewById(R.id.newyorktime);
+        newyorkTime.setText(timeN);
     }
 
-    //Load daa on app start
+    //Load data on app start
     public void start(){
+        DateFormat twelveHour = new SimpleDateFormat("hh:mm aa", Locale.ENGLISH);
+        twelveHour.setTimeZone(sydney);
+
+        String timeS = twelveHour.format(s.getTime());
         sydneyTime = findViewById(R.id.sydneytime);
-        sydneyTime.setText(sydneyTimeString);
+        sydneyTime.setText(timeS);
 
+        twelveHour.setTimeZone(tokyo);
+
+        String timeT = twelveHour.format(t.getTime());
         tokyoTime = findViewById(R.id.tokyotime);
-        tokyoTime.setText(tokyoTimeString);
+        tokyoTime.setText(timeT);
 
+        twelveHour.setTimeZone(auckland);
+
+        String timeA = twelveHour.format(a.getTime());
         aucklandTime = findViewById(R.id.aucklandtime);
-        aucklandTime.setText(aucklandTimeString);
+        aucklandTime.setText(timeA);
 
+        twelveHour.setTimeZone(dubai);
+
+        String timeD = twelveHour.format(d.getTime());
         dubaiTime = findViewById(R.id.dubaitime);
-        dubaiTime.setText(dubaiTimeString);
+        dubaiTime.setText(timeD);
 
+        twelveHour.setTimeZone(newyork);
+
+        String timeN = twelveHour.format(n.getTime());
         newyorkTime = findViewById(R.id.newyorktime);
-        newyorkTime.setText(newyorkTimeString);
+        newyorkTime.setText(timeN);
+
     }
 
     //Refresh app button logic
